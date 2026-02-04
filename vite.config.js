e197d872f966c54017299d69bf6984a475b142bc
Yihan <since123e@gmail.com>
2026-02-04 17:38:18 +0800

    去掉仓库路径

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ const htmlPartials = () => ({
 });
 
 export default {
-  base: '/Agentic-AI-Lab-AAIL-Website/',
+  // base: '/Agentic-AI-Lab-AAIL-Website/',
   root: 'src',
   publicDir: '../public',
   plugins: [htmlPartials()],
